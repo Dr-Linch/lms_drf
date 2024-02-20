865fdc02d41e21d1f87a1d17c30ca982cff6335d
Linch <dr.linch19@gmail.com>
2024-02-20 21:06:42 +0300

    HW 25.2

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from users.models import User
 from users.serializers import UserSerializer
 
 
-# Create your views here.
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
